fc2fb614732e791845d064ea3503e5e3c6c03165
Collin <collinmc1228@gmail.com>
2023-02-18 19:29:11 -0500

    update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <>
-
+      <div className="rounded-xl bg-purple-700 m-8 p-8 w-fit h-fit">
+        <h1 className="font-primary text-8xl text-fuchsia-600">Invisible</h1>
+        <h1 className="font-primary text-4xl text-fuchsia-600">A Simple Discord App</h1>
+      </div>
     </>
   )
 }
